878f20864387b0fb529300ded2df3f45e638dc4c
Bogdan <lordeldrus@gmail.com>
2021-03-11 23:36:02 +0200

    Create Account.java

---
+++ src/main/java/edu/kpi/testcourse/auth/Account.java
@@ -0,0 +1,46 @@
+package edu.kpi.testcourse.auth;
+
+/**
+ Аккаунт имееться введу юзер.
+ */
+public class Account {
+  private int id;
+  private String email;
+  private String hashPass;
+
+  /*
+   Конструктор для email и hashPass
+   */
+  public Account(String email, String hashPass) {
+    this.email = email;
+    this.hashPass = hashPass;
+  }
+
+  /*
+   Меняем id
+   */
+  public void setId(int i) {
+    this.id = i;
+  }
+
+  /*
+   Получаем id
+   */
+  public int getId() {
+    return id;
+  }
+
+  /*
+   Получем email
+   */
+  public String getAccountEmail() {
+    return email;
+  }
+
+  /*
+   Получаем hashPass
+   */
+  public String getAccountHashPass() {
+    return hashPass;
+  }
+}
